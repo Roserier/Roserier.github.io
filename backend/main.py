e697bedce33cddf141d4fee0d12e669d5089eaa0
Roserier <danang.ak17@gmail.com>
2022-07-14 22:34:52 +0700

    licik

---
+++ backend/main.py
@@ -0,0 +1,10 @@
+import json
+from flask import Flask, jsonify, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def main():
+
+    return jsonify({'hasil' : 'HAHA','status':'1'})

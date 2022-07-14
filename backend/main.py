ba2a15a9b14d6130a13f6c788450e2b69490dc0c
Roserier <66874503+Roserier@users.noreply.github.com>
2022-07-14 22:50:16 +0700

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -4,7 +4,10 @@ from flask import Flask, jsonify, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])
+@app.route('/api',methods=['GET'])
 def main():
-
     return jsonify({'hasil' : 'HAHA','status':'1'})
+
+
+if __name__ == '__main__':
+    app.run(debug=False")
